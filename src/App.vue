c7c06f8adde3e15e923a6ef3b5f2f2a279fc088c
Kingsley <kifeanyi216@gmail.com>
2026-02-10 17:09:43 +0100

    Hero Section

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     class="min-h-screen bg-netflix-dark text-white font-sans overflow-x-hidden"
   >
     <AppHeader />
+    <HeroBanner :featured="featuredContent" />
   </div>
 </template>
 
@@ -11,6 +12,17 @@ import { ref, onMounted } from "vue";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import AppHeader from "./components/AppHeader.vue";
+import HeroBanner from "./components/HeroBanner.vue";
+
+const featuredContent = ref({
+  title: "Shang-Chi and the Legend of the Ten Rings",
+  description: `Shang-Chi must confront the past he thought he left behind when he is drawn into the web of the mysterious Ten Rings organization.`,
+  imageUrl: "/shangchi.webp",
+  genres: ["Action", "Adventure", "Sci-Fi"],
+  year: 2022,
+  seasons: 2,
+  rating: 8.9,
+});
 
 onMounted(() => {
   AOS.init({
